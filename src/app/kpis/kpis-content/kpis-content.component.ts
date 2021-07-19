14410bca78f554d4a8cd18e8cb4f786c61552a82
diego dorado <diegodorado78@gmail.com>
2021-07-19 13:21:15 -0500

    funcional kpis con estilos

--- src/app/kpis/kpis-content/kpis-content.component.ts
+++ src/app/kpis/kpis-content/kpis-content.component.ts
@@ -8,8 +8,22 @@ import { LinesService } from 'src/app/core/services/lines.service';
 })
 export class KpisContentComponent implements OnInit {
 line:any = {};
+upLabel1:string='';
+upLabel2:string='';
+upLabel3:string='';
+
+upUnid:string='';
+upUnid2:string='';
+
+downLabel1:string='';
+downLabel2:string='';
+downLabel3:string='';
+
+downUnid:string='';
+downUnid2:string='';
+
 public bar:any= document.getElementById('lineData');
-size:string='33'
+size:string=''
 constructor( private route: ActivatedRoute, private linesService: LinesService) // inyeccion de dependencias activatedroute) { }
 {}
   ngOnInit(): void {
@@ -19,4 +33,47 @@ constructor( private route: ActivatedRoute, private linesService: LinesService) 
       // llamo al metodo get product del service
     }); //
   }
+  // metodos para actualizar la data
+  updateYield(){
+    this.upLabel1='4,215'
+    this.upLabel2= ''
+    this.upLabel3= 'Total Material'
+    this.upUnid="kg"
+    this.upUnid2=''
+    this.downLabel1= '1,270'
+    this.downLabel2=''
+    this.downLabel3='Good Roll'
+    this.downUnid="kg"
+    this.downUnid2=''
+    this.size='22.5'
+  }
+  updateUptime(){
+    this.upLabel1='893'
+    this.upLabel2= '09'
+    this.upLabel3= 'Total Time'
+    this.upUnid="d"
+    this.upUnid2="h"
+    this.downLabel1= '893'
+    this.downLabel2= '07'
+    this.downLabel3='Production'
+    this.downUnid="d"
+    this.downUnid2="h"
+     this.size='75'
+  }
+  updateCapacity(){
+    this.upLabel1='6,160'
+    this.upLabel2= ''
+    this.upLabel3= 'Normal Output'
+    this.upUnid="kg/h"
+    this.upUnid2=""
+    this.downLabel1= '4,176'
+    this.downLabel2= ''
+    this.downLabel3='Output on Winder'
+    this.downUnid="kg/h"
+    this.downUnid2=''
+    this.size='51'
+
+  }
+
+
 }
