551c54c5fb0a92082c687ee386f61afd835cef2c
diego dorado <diegodorado78@gmail.com>
2022-03-09 19:29:33 -0500

    funcional botones zoom

--- src/app/reports/winder/components/chart1/chart1.component.ts
+++ src/app/reports/winder/components/chart1/chart1.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit,ElementRef,ViewChild, OnChanges, Input } from '@angular/core';
+import { Component, OnInit,ElementRef,ViewChild} from '@angular/core';
 import { WinderService } from 'src/app/core/services/winder.service';
 import { Chart, registerables } from 'chart.js';
 import zoomPlugin from 'chartjs-plugin-zoom';
 Chart.register(zoomPlugin);
-// import * as d3 from 'd3';
 import {Response} from '../../../../response.model'
 
 
@@ -14,42 +13,31 @@ import {Response} from '../../../../response.model'
 })
 export class Chart1Component implements OnInit {
 
-public chartTitle="patito";
+public chartTitle="Thickness Control";
 winderData:Response[];
 data1:any;
 data2:any;
 data3:any;
-
-//  @Input() filmTensionData:Response[];
-//  @ViewChild("chart", { static: true }) protected chartContainer: ElementRef;
-  // svg: any;
-  // g: any;
-  // tooltip: any;
-  // margin: { top: number; right: number; bottom: number; left: number; };
-  // contentWidth: number;
-  // contentHeight: number;
-  // width: number;
-  // height: number;
-  // n:any=[];
-
-  myChart: any;
-
+chart:any
+myChart: any;
+enableState:boolean;
+stateMessage:String;
+enableButton:any;
+// @ViewChild("enableButton") enableButton: ElementRef;
 
   constructor(private winderService:WinderService) {
     this.winderData = this.winderService.getAllDataPoints();
     this.data1 =this.winderData.map(film=>{return film.rollId});
     this.data2 =this.winderData.map(film=>{return film.filmTension});
     this.data3 =this.winderData.map(film=>{return film.actual});
+
   }
 
   ngOnInit():void {
-    this.myChart=document.getElementById('canvas')
     Chart.register(...registerables);
-    this.loadChart();
-  }
-
-    loadChart(){
-     new Chart(this.myChart,{
+    this.myChart=document.getElementById('canvas');
+    this.enableButton=document.getElementById('enableButton')
+    this.chart= new Chart(this.myChart,{
       type: 'line',
       data: {
           labels: this.data1 ,
@@ -117,82 +105,27 @@ data3:any;
         }
       },
     })
+     this.setState()
+  }
+  resetZoom(){
+   this.chart.resetZoom();
+  }
 
-  //  fetchData=()=>{
-  //   this.winderService.getAllDataPoints()
-  //   .subscribe((response:Response[])=>{
-  //      this.winderData = response['data'];
-  //     console.log(this.winderData)
-  //   })
-  //   return this.winderData
-  // }
+  setState(){
+    this.enableState=this.chart.options.plugins.zoom.zoom.wheel.enabled;
+  if(this.enableState){
+    return this.stateMessage="Enabled"
+  }else{
+  return this.stateMessage="Disabled";
+  }
 
-// // primer metodo para crear el contenedor del chart
-//   initChart() {
-//     const element = this.chartContainer.nativeElement;
-//     this.svg = d3.select(element);
-//     this.margin = {
-//       top: +this.svg.style("margin-top").replace("px", ""),
-//       right: +this.svg.style("margin-right").replace("px", ""),
-//       bottom: +this.svg.style("margin-bottom").replace("px", ""),
-//       left: +this.svg.style("margin-left").replace("px", "")
-//     };
-//     this.width = +this.svg.style("width").replace("px", "");
-//     this.height = +this.svg.style("height").replace("px", "");
-//     this.contentWidth = this.width - this.margin.left - this.margin.right;
-//     this.contentHeight = this.height - this.margin.top - this.margin.bottom;
-//     this.g = this.svg.append("g").attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-//   }
-//   genChart(){
-//   }
-//  createChart() {
-//     var data =  this.filmTensionData
-//     console.log(data)
-//     var xScale = d3.scalePoint()
-//         .domain(data.map((d:any) => d.rollId))
-//         .range([0,this.contentWidth])
+  }
+  enableZoom(){
+    this.chart.options.plugins.zoom.zoom.wheel.enabled = !this.chart.options.plugins.zoom.zoom.wheel.enabled;
+    this.setState();
+    this.chart.update();
 
-//     var yScale = d3.scaleLinear()
-//       .domain([0, Math.max.apply(Math, data.map(roll => roll.filmTension))])
-//       .range([this.contentHeight, 0]);
-
-//       var line = d3.line()
-//       .x(function (d:any, i:any) { return xScale(d.x); })
-//       .y(function (d: any) { return yScale(d.y); })
-//       .curve(d3.curveMonotoneX)
-
-//       var dataset =data.map((roll)=>{
-//       return {y:roll.filmTension,x:roll.rollId }})
-
-//     this.g.append("g")
-//       .attr("class", "x axis")
-//       .attr("transform", "translate(" + 0 + "," + this.contentHeight + ")")
-//       .call(d3.axisBottom(xScale));
-
-//     this.g.selectAll("text")
-//     .attr("transform", "translate(-10,0)rotate(-90)")
-//     .style("text-anchor", "end")
-//     .style("font-size",".8em");
-
-//     this.g.append("g")
-//       .attr("class", "y axis")
-//       .call(d3.axisLeft(yScale));
-
-//     this.g.append("path")
-//       .datum(dataset )
-//       .attr("class", "winder__c1-line1")
-//       .attr("d", line);
-
-//     this.g.selectAll(".dot")
-//       .data(dataset)
-//       .enter().append("circle")
-//       .attr("class", "winder__c1-dot")
-//       .attr("cx", function (d, i) { return xScale(d.x) })
-//       .attr("cy", function (d) { return yScale(d.y) })
-//       .attr("r", 3)
-//   }
+  }
 }
-resetZoom(){
-  this.myChart.resetZoom()
-}
-}
+
+
