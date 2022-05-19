1a46acce83a45bda08887fbecdd17a6366bd314e
diego.dorado <diego.dorado@brueckner.com>
2022-05-19 16:53:30 -0500

    rxjs updated

--- src/app/core/services/dates.service.ts
+++ src/app/core/services/dates.service.ts
@@ -8,10 +8,20 @@ import { BehaviorSubject, Subject } from 'rxjs';
 export class DatesService {
 
   constructor() { }
-  public editDataDetails: any = [];public subject = new Subject<any>();
-  private messageSource = new  BehaviorSubject(this.editDataDetails);
-  currentMessage = this.messageSource.asObservable();
-  changeMessage(message: string) {
-    this.messageSource.next(message)
+  public startDateDetails: any = [];
+  public endDateDetails: any = [];
+
+  public startDateSubject = new Subject<any>();
+  public endDateSubject = new Subject<any>();
+  private startDateSource = new BehaviorSubject(this.startDateDetails);//debe tener un valor inicial en este caso un string vacio
+  private endDateSource = new BehaviorSubject(this.endDateDetails);//debe tener un valor inicial en este caso un string vacio
+
+  currentStartDate = this.startDateSource.asObservable();
+  currentEndDate = this.endDateSource.asObservable();
+
+  changeMessage(startMessage: string, endMessage: string) {
+    this.startDateSource.next(startMessage)
+    this.endDateSource.next(endMessage)
+
   }
 }
