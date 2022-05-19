63a3953c58946c41372275c382189f9491f9f763
diego.dorado <diego.dorado@brueckner.com>
2022-05-19 16:22:25 -0500

    conexion components

---
+++ src/app/core/services/dates.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DatesService {
+
+  constructor() { }
+  public editDataDetails: any = [];public subject = new Subject<any>();
+  private messageSource = new  BehaviorSubject(this.editDataDetails);
+  currentMessage = this.messageSource.asObservable();
+  changeMessage(message: string) {
+    this.messageSource.next(message)
+  }
+}
